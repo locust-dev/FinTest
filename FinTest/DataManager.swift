6e66f55fbd386776adf249e05b52c3712dac215b
locust-dev <rrrr.ty30@gmail.com>
2021-07-25 16:07:11 +0300

    Fixed some constraints

---
+++ FinTest/DataManager.swift
@@ -0,0 +1,16 @@
+//
+//  DataManager.swift
+//  FinTest
+//
+//  Created by Илья Тюрин on 25.07.2021.
+//
+
+class DataManager {
+    
+    static let shared = DataManager()
+    private init() {}
+    
+    let titles = ["Some", "Up", "Down"]
+    let subtitles = ["Main screen", "Detail Screen", "Work Item"]
+    
+}
--- FinTest/MainViewController.swift
+++ FinTest/MainViewController.swift
@@ -23,23 +23,24 @@ class MainViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        DataManager.shared.titles.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! InfoCell
 
-        // Configure the cell...
+        cell.mainTitle.text = DataManager.shared.titles[indexPath.row]
+        cell.subtitle.text = DataManager.shared.subtitles[indexPath.row]
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
